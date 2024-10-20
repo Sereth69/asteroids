af1025ad6910829676eef08dca5ee017e77f93c2
Sereth69 <damitnoob@hotmail.com>
2024-10-20 20:39:30 +1300

    deploying asteroids

---
+++ asteroid.py
@@ -0,0 +1,25 @@
+import pygame
+import random
+from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        random_angle = random.uniform(20, 50)
+        velocity1 = self.velocity.rotate(random_angle) * 1.2
+        velocity2 = self.velocity.rotate(-random_angle) * 1.2
+        Asteroid(self.position.x, self.position.y, new_radius).velocity = velocity1
+        Asteroid(self.position.x, self.position.y, new_radius).velocity = velocity2
